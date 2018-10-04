90c221837cf325b53617f9238d52978b40ed5238
Jeff Coffler <jeff@taltos.com>
2018-10-04 09:24:40 -0700

    Modify backup configuration tests to be quiet, check for errors
    Backup configuration tests tended to be noisy. While correct, it took
    study of the output to verify so.
    
    Modifed tests to be quiet (no debug output), and to check correctness
    of the results at run-time, making for more robust unit tests.

--- configBackup_test.go
+++ configBackup_test.go
@@ -1,55 +1,199 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestValidConfigWithNumberedKeys(t *testing.T) {
+	quietFlag = true
+	defer func() {
+		quietFlag = false
+	}()
+
+	// Read the expected configuration file under test
 	configFile = newConfigurationFile()
 	configFile.setConfig("numberedKeys")
 	globalStorageDirectory = "test/assets/backupConfigs/"
-	err := configFile.loadConfig(true, true)
-	if err != nil {
+	if err := configFile.loadConfig(false, false); err != nil {
 		t.Error(err)
+	}
+
+	// Verify results of the configuration file load for backupInfo
+	var backupInfo = []map[string]string{
+		{"name": "b2", "threads": "10", "vssFlag": "false", "vssTimeout": "false"},
+		{"name": "azure-direct", "threads": "5", "vssFlag": "false", "vssTimeout": "false"},
+		{"name": "default-threads", "threads": "1", "vssFlag": "false", "vssTimeout": "false"},
+	}
+
+	if reflect.DeepEqual(backupInfo, configFile.backupInfo) == false {
+		t.Error("backupInfo should have been equal, expected:", backupInfo, ", received:", configFile.backupInfo)
+	}
+
+	// Verify results of the configuration file load for copyInfo
+	var copyInfo = []map[string]string{
+		{"from": "b2", "to": "azure", "threads": "10"},
+		{"from": "b2", "to": "default-threads", "threads": "1"},
+	}
+
+	if reflect.DeepEqual(copyInfo, configFile.copyInfo) == false {
+		t.Error("copyInfo should have been equal, expected:", copyInfo, ", received:", configFile.copyInfo)
+	}
+
+	// Verify results of the configuration file load for pruneInfo
+	var pruneInfo = []map[string]string{
+		{"storage": "b2", "keep": "-keep 0:365 -keep 30:180 -keep 7:30 -keep 1:7"},
+		{"storage": "azure", "keep": "-keep 0:365 -keep 30:180 -keep 7:30 -keep 1:7"},
+	}
+
+	if reflect.DeepEqual(pruneInfo, configFile.pruneInfo) == false {
+		t.Error("pruneInfo should have been equal, expected:", pruneInfo, ", received:", configFile.pruneInfo)
+	}
+
+	// Verify results of the configuration file load for checkInfo
+	var checkInfo = []map[string]string{
+		{"storage": "b2", "all": "true"},
+		{"storage": "azure", "all": "false"},
+	}
+
+	if reflect.DeepEqual(checkInfo, configFile.checkInfo) == false {
+		t.Error("pruneInfo should have been equal, expected:", checkInfo, ", received:", configFile.checkInfo)
 	}
 }
 
 func TestValidConfigWithArray(t *testing.T) {
+	quietFlag = true
+	defer func() {
+		quietFlag = false
+	}()
+
+	// Read the expected configuration file under test
 	configFile = newConfigurationFile()
 	configFile.setConfig("array")
 	globalStorageDirectory = "test/assets/backupConfigs/"
-	err := configFile.loadConfig(true, true)
-	if err != nil {
+	if err := configFile.loadConfig(false, false); err != nil {
 		t.Error(err)
+	}
+
+	// Verify results of the configuration file load for backupInfo
+	var backupInfo = []map[string]string{
+		{"name": "b2", "threads": "10", "vssFlag": "false", "vssTimeout": "false"},
+		{"name": "azure-direct", "threads": "5", "vssFlag": "false", "vssTimeout": "false"},
+		{"name": "default-threads", "threads": "1", "vssFlag": "false", "vssTimeout": "false"},
+	}
+
+	if reflect.DeepEqual(backupInfo, configFile.backupInfo) == false {
+		t.Error("backupInfo should have been equal, expected:", backupInfo, ", received:", configFile.backupInfo)
+	}
+
+	// Verify results of the configuration file load for copyInfo
+	var copyInfo = []map[string]string{
+		{"from": "b2", "to": "azure", "threads": "10"},
+		{"from": "b2", "to": "default-threads", "threads": "1"},
+	}
+
+	if reflect.DeepEqual(copyInfo, configFile.copyInfo) == false {
+		t.Error("copyInfo should have been equal, expected:", copyInfo, ", received:", configFile.copyInfo)
+	}
+
+	// Verify results of the configuration file load for pruneInfo
+	var pruneInfo = []map[string]string{
+		{"storage": "b2", "keep": "-keep 0:365 -keep 30:180 -keep 7:30 -keep 1:7"},
+		{"storage": "azure", "keep": "-keep 0:365 -keep 30:180 -keep 7:30 -keep 1:7"},
+	}
+
+	if reflect.DeepEqual(pruneInfo, configFile.pruneInfo) == false {
+		t.Error("pruneInfo should have been equal, expected:", pruneInfo, ", received:", configFile.pruneInfo)
+	}
+
+	// Verify results of the configuration file load for checkInfo
+	var checkInfo = []map[string]string{
+		{"storage": "b2", "all": "true"},
+		{"storage": "azure", "all": "false"},
+	}
+
+	if reflect.DeepEqual(checkInfo, configFile.checkInfo) == false {
+		t.Error("pruneInfo should have been equal, expected:", checkInfo, ", received:", configFile.checkInfo)
 	}
 }
 
 func TestValidConfig_NoCopySection(t *testing.T) {
+	quietFlag = true
+	defer func() {
+		quietFlag = false
+	}()
+
+	// Read the expected configuration file under test
 	configFile = newConfigurationFile()
 	configFile.setConfig("noCopySection")
 	globalStorageDirectory = "test/assets/backupConfigs/"
-	err := configFile.loadConfig(true, true)
-	if err != nil {
+	if err := configFile.loadConfig(false, false); err != nil {
 		t.Error(err)
+	}
+
+	// Verify results of the configuration file load for backupInfo
+	var backupInfo = []map[string]string{
+		{"name": "b2", "threads": "10", "vssFlag": "false", "vssTimeout": "false"},
+		{"name": "azure-direct", "threads": "5", "vssFlag": "false", "vssTimeout": "false"},
+		{"name": "default-threads", "threads": "1", "vssFlag": "false", "vssTimeout": "false"},
+	}
+
+	if reflect.DeepEqual(backupInfo, configFile.backupInfo) == false {
+		t.Error("backupInfo should have been equal, expected:", backupInfo, ", received:", configFile.backupInfo)
+	}
+
+	// Verify results of the configuration file load for copyInfo (this test has no Copy section)
+	var copyInfo []map[string]string
+
+	if reflect.DeepEqual(copyInfo, configFile.copyInfo) == false {
+		t.Error("copyInfo should have been equal, expected:", copyInfo, ", received:", configFile.copyInfo)
+	}
+
+	// Verify results of the configuration file load for pruneInfo
+	var pruneInfo = []map[string]string{
+		{"storage": "b2", "keep": "-keep 0:365 -keep 30:180 -keep 7:30 -keep 1:7"},
+		{"storage": "azure", "keep": "-keep 0:365 -keep 30:180 -keep 7:30 -keep 1:7"},
+	}
+
+	if reflect.DeepEqual(pruneInfo, configFile.pruneInfo) == false {
+		t.Error("pruneInfo should have been equal, expected:", pruneInfo, ", received:", configFile.pruneInfo)
+	}
+
+	// Verify results of the configuration file load for checkInfo
+	var checkInfo = []map[string]string{
+		{"storage": "b2", "all": "true"},
+		{"storage": "azure", "all": "false"},
+	}
+
+	if reflect.DeepEqual(checkInfo, configFile.checkInfo) == false {
+		t.Error("pruneInfo should have been equal, expected:", checkInfo, ", received:", configFile.checkInfo)
 	}
 }
 
 func TestInvalidConfig_MissingStorage(t *testing.T) {
+	quietFlag = true
+	defer func() {
+		quietFlag = false
+	}()
+
 	configFile = newConfigurationFile()
 	configFile.setConfig("missingStorage")
 	globalStorageDirectory = "test/assets/backupConfigs/"
-	err := configFile.loadConfig(true, true)
-	if err == nil {
+	if err := configFile.loadConfig(false, false); err == nil {
 		t.Error("Invalid storage configuration error should have been returned")
 	}
 }
 
 func TestInvalidConfig_MissingStorageName(t *testing.T) {
+	quietFlag = true
+	defer func() {
+		quietFlag = false
+	}()
+
 	configFile = newConfigurationFile()
 	configFile.setConfig("missingStorageName")
 	globalStorageDirectory = "test/assets/backupConfigs/"
-	err := configFile.loadConfig(true, true)
-	if err == nil {
+	if err := configFile.loadConfig(false, false); err == nil {
 		t.Error("Invalid storage configuration error should have been returned")
 	}
 }
